5434e65edc0185a825b46e20709fdc147d35b3f3
Nicolas Spalinger <nicolas.spalinger@gmail.com>
2010-11-29 10:22:38 +0000

    Fix paths and improve build script

--- puritan/src/generate.py
+++ puritan/src/generate.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python
 #
-# When SFDs are in current directory, run this with
+# With the SFDs in the current directory, run this with
 # $ python generate.py
 
-import fontforge
+import fontforge, sys
+required_version = "20090923"
+
+if fontforge.version() < required_version:
+  print ("Your version of FontForge is too old - %s or newer is required" % (required_version));
+
+print ("Current fontforge version:")
+print fontforge.version()
 
 files = [
-'PuritanBoldItalic.sfd',
-'PuritanBold.sfd',
-'PuritanItalic.sfd',
-'Puritan.sfd'
+	'Puritan-BoldItalic.sfd',
+	'Puritan-Bold.sfd',
+	'Puritan-Italic.sfd',
+	'Puritan-Regular.sfd'
 ]
 
 for font in files:
-    f = fontforge.open(font)
-    f.generate(f.fontname + '.otf')
+    f = fontforge.open(font)    
+    print ("Building   ") + f.fullname + ( " ") + f.weight + ("  from sfd sources with fontforge")
     f.em = 1024
     f.round()
     f.selection.all()
     f.autoHint()
     f.autoInstr()
-    f.generate(f.fontname + '.ttf')
+    f.generate(f.fontname + ".otf",flags=("opentype"))
+    f.close
+
+print ("font version:")
+print (f.version)
+
+print ("Done");
+
