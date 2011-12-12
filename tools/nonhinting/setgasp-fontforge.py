e3b2ad0263c87f3682b8894c645826284e870eeb
Dave Crossland <dave@lab6.com>
2011-12-12 14:20:43 -0800

    Adding Apache 2.0 license to some magic hinting scripts

--- tools/nonhinting/setgasp-fontforge.py
+++ tools/nonhinting/setgasp-fontforge.py
@@ -2,7 +2,20 @@
 #
 # setgasp-fontforge.py
 #
-# Copyright (c) 2011 Dave Crossland <dave@understandinglimited.com>
+# Copyright 2011, Google Inc.
+# Author: Dave Crossland (dave@understandinglimited.com)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License. 
 #
 # This program takes a TTF font sets its GASP table to
 # magic value 15 in Version 1 that turn on rendering features that 
