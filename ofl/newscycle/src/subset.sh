d4b38e197f64326a9780b3ba1eedfc1ab5bb579b
Dave Crossland <dave@lab6.com>
2012-04-26 15:16:31 -0700

    Updating News Cycle to 0.502 to fix PS Names

--- ofl/newscycle/src/subset.sh
+++ ofl/newscycle/src/subset.sh
@@ -2,14 +2,14 @@ cd ~/src/font-optimizer; \
 ./subset.pl -v --licensesubst="SIL OFL 1.1" \
  --exclude="c2sc,smcp,ordn,sups,sinf,subs,dlig,frac" \
  --chars="News Cycle" \
-~/googlefontdirectory/newscycle/NewsCycle-Regular.{ttf,menu}; \
-cd ~/googlefontdirectory/newscycle/; \
+~/googlefontdirectory/ofl/newscycle/NewsCycle-Regular.{ttf,menu}; \
+cd ~/googlefontdirectory/ofl/newscycle/; \
 ~/googlefontdirectory/tools/chars/showchars.py \
  NewsCycle-Regular.menu; 
 
-cd ~/googlefontdirectory/newscycle/; \
+cd ~/googlefontdirectory/ofl/newscycle/; \
 rm *.nam *.log *.latin *.latin-ext *.cyrillic *.cyrillic-ext *.greek *.greek-ext; \
-cd ~/googlefontdirectory/; \
+cd ~/googlefontdirectory/ofl/; \
 for family in newscycle; do \
    for font in `ls -1 $family/*ttf | cut -d. -f1`; do \
        for subset in \
@@ -24,7 +24,7 @@ for family in newscycle; do \
        done; \
    done; \
 done; \
-cd ~/googlefontdirectory/newscycle/; \
+cd ~/googlefontdirectory/ofl/newscycle/; \
 rename s/cyrillic\\+latin/cyrillic/g *; \
 rename s/cyrillic-ext\\+latin/cyrillic-ext/g *; \
 rename s/greek\\+latin/greek/g *; \
