a53405d1c0f0483c207926eca694decf752f1b4a
Dave Crossland <dave@lab6.com>
2012-05-14 06:37:20 -0700

    Adding Apache license to genmetadata

--- tools/genmetadata/genmetadata.py
+++ tools/genmetadata/genmetadata.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python2.6
+#
+# Copyright 2012, Google Inc.
+# Author: Jeremie Lenfant-Engelmann (jeremiele a google com)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# A script for generating METADATA.json files, using FontForge
+#
+# DC Q: what and where is size calculated?
 
 from datetime import date
 
