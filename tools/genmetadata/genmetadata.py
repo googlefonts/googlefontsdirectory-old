e9ddabd3bbf32f8cebe7b2e9052516d510fed5bc
Dave Crossland <dave@lab6.com>
2012-05-15 01:03:18 -0700

    Adding ansiprint of fontTools extracted strings to genmetadata.py

--- tools/genmetadata/genmetadata.py
+++ tools/genmetadata/genmetadata.py
@@ -257,15 +257,21 @@ def createFonts(familydir, familyname):
     # DC This was previously done with FontForge
     # fontmetadata["name"] = familyname
       fontmetadata["name"] = fontToolsGetFamilyName(ftfont)
+      ansiprint("name from font is: " + fontmetadata["name"], "green")
       fontmetadata["style"] = inferStyle(ffont)
+      ansiprint("style from font is: " + fontmetadata["style"], "green")
       fontmetadata["weight"] = ffont.os2_weight
+      ansiprint("weight from font is: " + str(fontmetadata["weight"]), "green")
       fontmetadata["filename"] = f
+      ansiprint("filename from font is: " + fontmetadata["filename"], "green")
     # DC This was previously done with FontForge
     # fontmetadata["postScriptName"] = ffont.fontname
       fontmetadata["postScriptName"] = fontToolsGetPSName(ftfont)
+      ansiprint("postScriptName from font is: " + fontmetadata["postScriptName"], "green")
     # DC This was previously done with FontForge
     # fontmetadata["fullName"] = ffont.fullname
       fontmetadata["fullName"] = fontToolsGetFullName(ftfont)
+      ansiprint("fullname from font is: " + fontmetadata["fullName"], "green")
       fonts.append(fontmetadata)
   return fonts
 
@@ -311,6 +317,7 @@ def getDesigner(familydir):
           color = "green"
           ansiprint(string, color)
           return desName
+
 
 def genmetadata(familydir):
   metadata = InsertOrderedDict()
