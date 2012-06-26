7ca0db2c14c2228fc5c05450898bdce05955730f
Dave Crossland <dave@lab6.com>
2012-06-26 02:44:12 +0100

    Updaing Roboto files, adding Vietnamese subset

--- roboto/src/autohint-roboto.sh
+++ roboto/src/autohint-roboto.sh
@@ -16,6 +16,25 @@
 # Since the old Roboto values are now the default, it has
 # become much simpler
 
+ttfautohint --verbose -f Roboto-Black.ttf Roboto-Black-TA.ttf;
+ttfautohint --verbose -f Roboto-BlackItalic.ttf Roboto-BlackItalic-TA.ttf;
+ttfautohint --verbose -f Roboto-Bold.ttf Roboto-Bold-TA.ttf;
+ttfautohint --verbose -f Roboto-BoldItalic.ttf Roboto-BoldItalic-TA.ttf;
+ttfautohint --verbose -f Roboto-Italic.ttf Roboto-Italic-TA.ttf;
+ttfautohint --verbose -f Roboto-Light.ttf Roboto-Light-TA.ttf;
+ttfautohint --verbose -f Roboto-LightItalic.ttf Roboto-LightItalic-TA.ttf;
+ttfautohint --verbose -f Roboto-Medium.ttf Roboto-Medium-TA.ttf;
+ttfautohint --verbose -f Roboto-MediumItalic.ttf Roboto-MediumItalic-TA.ttf;
+ttfautohint --verbose -f Roboto-Regular.ttf Roboto-Regular-TA.ttf;
+ttfautohint --verbose -f Roboto-Thin.ttf Roboto-Thin-TA.ttf;
+ttfautohint --verbose -f Roboto-ThinItalic.ttf Roboto-ThinItalic-TA.ttf;
+ttfautohint --verbose -f RobotoCondensed-Bold.ttf RobotoCondensed-Bold-TA.ttf;
+ttfautohint --verbose -f RobotoCondensed-BoldItalic.ttf RobotoCondensed-BoldItalic-TA.ttf;
+ttfautohint --verbose -f RobotoCondensed-Italic.ttf RobotoCondensed-Italic-TA.ttf;
+ttfautohint --verbose -f RobotoCondensed-Light.ttf RobotoCondensed-Light-TA.ttf;
+ttfautohint --verbose -f RobotoCondensed-LightItalic.ttf RobotoCondensed-LightItalic-TA.ttf;
+ttfautohint --verbose -f RobotoCondensed-Regular.ttf RobotoCondensed-Regular-TA.ttf;
+
 for i; do
   infile=`basename $i`
   outfile=`echo $infile | sed 's/.ttf/-TA.ttf/'`
@@ -32,3 +51,4 @@ for i; do
   ttx $ttxfile
   rm $ttxfile
 done
+
