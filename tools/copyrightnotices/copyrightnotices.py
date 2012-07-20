d10187a59114c6b5a9b90d991552d9e67a85c397
Dave Crossland <dave@lab6.com>
2012-07-20 20:23:05 +0000

    minor edit to copyrightnotices.py

--- tools/copyrightnotices/copyrightnotices.py
+++ tools/copyrightnotices/copyrightnotices.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright 2012, Google Inc.
-# Author: Dave Crossland (dcrossland a google com )
+# Author: Dave Crossland (dcrossland a google com)
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
