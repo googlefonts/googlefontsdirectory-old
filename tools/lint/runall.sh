ede49392b68f25548e92b7c5b0b9bbf58566f90c
Jeremie Lenfant-Engelmann <tocman@gmail.com>
2012-09-18 16:05:32 -0700

    Run the lint tool for each directory instead of running it for all fonts at once.

--- tools/lint/runall.sh
+++ tools/lint/runall.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 cd $(dirname "${BASH_SOURCE[0]}")
-DIRECTORIES=""
-
-for metadata in $(find ../../{ofl,ufl,apache} -name METADATA.json); do
-  DIRECTORIES="$DIRECTORIES $(dirname $metadata)"
-done
 
 ant lint-jar
 
-JVM_ARGS="-Xmx1024m" java -jar dist/lint.jar $DIRECTORIES
+for metadata in $(find ../../{ofl,ufl,apache} -name METADATA.json); do
+  java -jar dist/lint.jar "$(dirname $metadata)"
+done
 
 cd - > /dev/null
